43ce53b236c2aeb559333c38324bc5b0465de464
Shayan Hashemi <83245719+Shayan5422@users.noreply.github.com>
2024-12-19 00:01:49 +0100

    log

--- audio-transcriber/src/app/app.config.ts
+++ audio-transcriber/src/app/app.config.ts
@@ -1,6 +1,6 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { appRoutes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
